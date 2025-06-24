a9d6e00f777794bec92da4dab55a19779ec64761
Harshdeep Sharma <40256677+harshaa765@users.noreply.github.com>
2025-06-24 07:08:46 +0530

    Update tailwind.config.js by adding dark mode feature

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
+  darkMode: 'class', // ✅ Enables class-based dark mode
   theme: {
     extend: {},
   },
-  plugins: [require('@tailwindcss/typography'), require('daisyui')],
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('daisyui')
+  ],
 };
