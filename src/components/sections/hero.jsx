60185c892af6290c970b713de2fb4f9d0841202e
Harshdeep Sharma <40256677+harshaa765@users.noreply.github.com>
2025-06-25 07:44:25 +0530

    Update hero.jsx for darktheme

--- src/components/sections/hero.jsx
+++ src/components/sections/hero.jsx
@@ -1,13 +1,14 @@
 import Link from 'next/link';
 import Image from 'next/image';
 
-// import profilePic from '../../../public/img/about-image.png';
 import profilePic from '../../../public/img/harshdeep_profile_photo.jpg';
-
 
 export default function Hero() {
   return (
-    <section id='home' className='hero min-h-screen'>
+    <section
+      id='home'
+      className='hero min-h-screen bg-white text-black dark:bg-[#0f172a] dark:text-white transition-colors duration-300'
+    >
       <div className='hero-content max-w-screen-2xl flex-col gap-4 px-2 lg:flex-row-reverse lg:px-4'>
         <Image
           src={profilePic}
@@ -16,15 +17,15 @@ export default function Hero() {
         />
         <div className='flex w-full flex-col items-center text-justify lg:w-1/2 lg:items-start'>
           <h1 className='py-4 text-start text-2xl font-bold'>
-            I’m a PhD researcher in Computational Mechanics, passionate about solving 
-            real-world problems at the intersection of numerical simulation, software engineering, 
+            I’m a PhD researcher in Computational Mechanics, passionate about solving
+            real-world problems at the intersection of numerical simulation, software engineering,
             and applied mathematics.
           </h1>
           <p className='py-2 text-xl font-medium'>
-            My research focuses on developing fatigue and fracture models for fiber-reinforced 
-            composites using phase-field methods, finite element modeling, and custom solvers 
-            through Abaqus UEL/UMAT, Python, FORTRAN, and C++. I also integrate commercial tools 
-            like Abaqus and COMSOL with open-source libraries like FEniCS, ParaView, and Gmsh to 
+            My research focuses on developing fatigue and fracture models for fiber-reinforced
+            composites using phase-field methods, finite element modeling, and custom solvers
+            through Abaqus UEL/UMAT, Python, FORTRAN, and C++. I also integrate commercial tools
+            like Abaqus and COMSOL with open-source libraries like FEniCS, ParaView, and Gmsh to
             create scalable simulation pipelines.
           </p>
           <Link
@@ -35,14 +36,14 @@ export default function Hero() {
             Contact Me
           </Link>
           <Link
-  href="/harshdeep_sharma_cv.pdf"
-  target="_blank"
-  rel="noopener noreferrer"
-  role="button"
-  className="btn btn-primary btn-wide my-2"
->
-  Download CV
-</Link>
+            href='/harshdeep_sharma_cv.pdf'
+            target='_blank'
+            rel='noopener noreferrer'
+            role='button'
+            className='btn btn-primary btn-wide my-2'
+          >
+            Download CV
+          </Link>
         </div>
       </div>
     </section>
